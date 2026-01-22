07cc07b7258049b326515300132e977b0b91937f
FabricioGregorio <120409359+FabricioGregorio@users.noreply.github.com>
2026-01-21 22:58:18 -0300

    feat: add campo List<ComprovanteResponseDTO> comprovantes no DTO de MovimentacaoResponse

--- src/main/java/br/edu/ifs/academico/DTO/movimentacao/response/MovimentacaoResponseDTO.java
+++ src/main/java/br/edu/ifs/academico/DTO/movimentacao/response/MovimentacaoResponseDTO.java
@@ -1,23 +1,26 @@
 package br.edu.ifs.academico.DTO.movimentacao.response;
 
 import br.edu.ifs.academico.DTO.status.response.StatusResponseDTO;
+import br.edu.ifs.academico.DTO.comprovante.response.ComprovanteResponseDTO;
+
+import java.util.List;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record MovimentacaoResponseDTO (
-        Long id,
-        BigDecimal valor,
-        Integer pontosCalculados,
-        LocalDate dataOcorrencia,
+public record MovimentacaoResponseDTO(
+                Long id,
+                BigDecimal valor,
+                Integer pontosCalculados,
+                LocalDate dataOcorrencia,
 
-        Long cartaoId,
-        String cartaoNome,
+                Long cartaoId,
+                String cartaoNome,
 
-        Long programaId,
-        String programaNome,
+                Long programaId,
+                String programaNome,
 
-        StatusResponseDTO status
+                StatusResponseDTO status,
 
-){
+                List<ComprovanteResponseDTO> comprovantes) {
 }
--- src/main/java/br/edu/ifs/academico/mapper/MovimentacaoMapper.java
+++ src/main/java/br/edu/ifs/academico/mapper/MovimentacaoMapper.java
@@ -2,8 +2,11 @@ package br.edu.ifs.academico.mapper;
 
 import br.edu.ifs.academico.DTO.movimentacao.response.MovimentacaoResponseDTO;
 import br.edu.ifs.academico.DTO.status.response.StatusResponseDTO;
+import br.edu.ifs.academico.DTO.comprovante.response.ComprovanteResponseDTO;
 import br.edu.ifs.academico.entity.MovimentacaoPontos;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class MovimentacaoMapper {
@@ -21,8 +24,11 @@ public class MovimentacaoMapper {
                 movi.getSaldo().getPrograma().getNome(),
                 new StatusResponseDTO(
                         movi.getStatus().getStatus(),
-                        movi.getStatus().getMotivo()
-                )
-        );
+                        movi.getStatus().getMotivo()),
+                movi.getComprovantes() != null ? movi.getComprovantes().stream().map(c -> new ComprovanteResponseDTO(
+                        c.getId(),
+                        c.getCaminho(),
+                        c.getTipo_arq(),
+                        c.getTamanho_bytes())).toList() : List.of());
     }
 }
